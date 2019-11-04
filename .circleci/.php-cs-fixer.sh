a75880889d9c3be91dd52403f86f134684aac37c
leiyu <leiyu@comsenz-service.com>
2019-11-04 15:25:29 +0800

    CIRCLE_COMMIT

--- .circleci/.php-cs-fixer.sh
+++ .circleci/.php-cs-fixer.sh
@@ -3,6 +3,6 @@ git config user.name "yulei745"
 git config user.email "yleimm@gmail.com"
 
 git add index.php src/* -f
-git commit -m "php-cs-fixer [circleci]"
+git commit -m "php-cs-fixer output for commit $CIRCLE_COMMIT [circleci]"
 
 git push $CIRCLE_REPOSITORY_URL
