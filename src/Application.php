f38452da58bfd89d9277f249bb035c93cd0d1d9b
yulei745 <yleimm@gmail.com>
2019-11-04 07:22:26 +0000

    php-cs-fixer [circleci]

--- src/Application.php
+++ src/Application.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Discuz & Tencent Cloud
+ * This is NOT a freeware, use is subject to license terms
+ *
+ * $Id: CreateThreadController.php xxx 2019-10-10 11:08:00 LiuDongdong $
+ */
+
 namespace Discuz\Foundation;
 
 use Illuminate\Container\Container;
