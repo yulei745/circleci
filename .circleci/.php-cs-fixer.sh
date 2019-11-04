1c05156eb594cda0d734d2c5a0e266dc2695c3d5
leiyu <leiyu@comsenz-service.com>
2019-11-04 14:37:03 +0800

    pushphpcode fixer

--- .circleci/.php-cs-fixer.sh
+++ .circleci/.php-cs-fixer.sh
@@ -2,7 +2,9 @@
 git config user.name "yulei745"
 git config user.email "yleimm@gmail.com"
 
-git add * -f
+git add index.php src/* -f
 git commit -m "php-cs-fixer [circleci]"
+
+echo $CIRCLE_BRANCH
 
 git push $CIRCLE_BRANCH
