9459ebaa452effe19a36e913790051a12387647f
leiyu <leiyu@comsenz-service.com>
2019-11-05 18:11:25 +0800

    git pull

--- .circleci/.php-cs-fixer.sh
+++ .circleci/.php-cs-fixer.sh
@@ -2,6 +2,7 @@
 git config user.name "discuz-bot"
 git config user.email "171550539@qq.com"
 
+git pull
 git add index.php src/* -f
 git commit -m "php-cs-fixer output for commit $CIRCLE_SHA1 [circleci]"
 
