c21adfe3276cef2f37a8bce6ef1398d0584c4e4f
yulei745 <yleimm@gmail.com>
2019-11-05 14:13:13 +0800

    test

--- .circleci/.php-cs-fixer.sh
+++ .circleci/.php-cs-fixer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git config user.name "yulei745"
-git config user.email "yleimm@gmail.com"
+git config user.name "discuz-bot"
+git config user.email "171550539@qq.com"
 
 git add index.php src/* -f
-git commit -m "php-cs-fixer output for commit $CIRCLE_COMMIT [circleci]"
+git commit -m "php-cs-fixer output for commit $CIRCLE_SHA1 [circleci]"
 
 git push $CIRCLE_REPOSITORY_URL
